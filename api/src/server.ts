6a61352f217010bc65976d9786c0245337bc338f
Diego Ferreira L.G.Oliveira <diegoferreira1964@gmail.com>
2025-10-12 18:36:38 -0300

    feat: criacao de servicos

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth';
 import adminRoutes from './routes/admin';
 import tecnicoRoutes from './routes/technical';
 import userRoutes from './routes/user';
+import serviceRoutes from './routes/service';
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/technical', tecnicoRoutes);
 app.use('/user', userRoutes);
+app.use('/services', serviceRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
