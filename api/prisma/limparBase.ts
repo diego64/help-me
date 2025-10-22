6c8501dfb29bf3eb6a6457a2f7db2fd3bb948d70
Diego Ferreira L.G. Oliveira <diegoferreira1964@gmail.com>
2025-10-22 20:22:33 -0300

    perf: melhoria na inicializacao da aplicacao e healthcheck no mongodb (#9)

--- api/prisma/limparBase.ts
+++ api/prisma/limparBase.ts
@@ -2,10 +2,18 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { PrismaClient } from '@prisma/client';
+import mongoose from 'mongoose';
+import ChamadoAtualizacaoModel from '../src/models/chamadoAtualizacao.model';
+
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.chamadoAtualizacao.deleteMany();
+  // Limpa histórico no MongoDB
+  await mongoose.connect(process.env.MONGO_URI!);
+  await ChamadoAtualizacaoModel.deleteMany({});
+  await mongoose.disconnect();
+
+  // Limpa dados nas tabelas SQL
   await prisma.ordemDeServico.deleteMany();
   await prisma.chamado.deleteMany();
   await prisma.servico.deleteMany();
@@ -15,7 +23,7 @@ async function main() {
 
 main()
   .then(() => {
-    console.log('Todos os dados foram excluídos!');
+    console.log('Todos os dados foram excluídos do MongoDB e PostgreSQL!');
     process.exit(0);
   })
   .catch((e) => {
