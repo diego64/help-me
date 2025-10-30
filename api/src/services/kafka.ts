58a27576a32935931ef1ebeda2ec7d402f35ef35
Diego Ferreira L.G. Oliveira <diegoferreira1964@gmail.com>
2025-10-30 13:08:42 -0300

    hotfix: correcao na inicializacao da api com o kafka (#13)
    Ao inicializar a API, o serviço do Kafka apresentava falhas de conexão, resultando na queda do container do Confluent Control Center e impedindo o funcionamento adequado da API. Esse comportamento evidenciou dependências críticas de inicialização e timing entre os serviços, comprometendo a disponibilidade do ambiente de mensageria e monitoramento. As correções implementadas visaram estabilizar a comunicação entre os componentes, garantir o startup ordenado dos containers e restabelecer a operação contínua dos sistemas integrados.

--- api/src/services/kafka.ts
+++ api/src/services/kafka.ts
@@ -1,28 +1,31 @@
-import { Kafka, logLevel, LogEntry } from 'kafkajs';
+  import { Kafka, logLevel, LogEntry } from 'kafkajs';
 
-const ignoreMessages = [
-  'The group is rebalancing, so a rejoin is needed'
-];
+  const ignoreMessages = [
+    'The group is rebalancing, so a rejoin is needed'
+  ];
 
-const customLogCreator = () => (entry: LogEntry) => {
-  const errorMsg = typeof entry.log?.error === 'string' ? entry.log.error : '';
-  if (
-    (entry.level === logLevel.ERROR || entry.level === logLevel.WARN) &&
-    !ignoreMessages.some(msg => errorMsg.includes(msg))
-  ) {
-    console.log(`[Kafka][${entry.label}]`, entry.log);
+  const customLogCreator = () => (entry: LogEntry) => {
+    const errorMsg = typeof entry.log?.error === 'string' ? entry.log.error : '';
+    if (
+      (entry.level === logLevel.ERROR || entry.level === logLevel.WARN) &&
+      !ignoreMessages.some(msg => errorMsg.includes(msg))
+    ) {
+      console.log(`[Kafka][${entry.label}]`, entry.log);
+    }
+  };
+
+  const brokerUrl = process.env.KAFKA_BROKER_URL;
+  if (!brokerUrl) throw new Error('KAFKA_BROKER_URL não definida!');
+
+  export const kafka = new Kafka({
+    clientId: 'helpdesk-api',
+    brokers: [brokerUrl],
+    logLevel: logLevel.ERROR,
+    logCreator: customLogCreator
+  });
+
+  export const producer = kafka.producer();
+
+  export async function conectarKafkaProducer() {
+    await producer.connect();
   }
-};
-
-export const kafka = new Kafka({
-  clientId: 'helpdesk-api',
-  brokers: ['localhost:9092'],
-  logLevel: logLevel.ERROR,
-  logCreator: customLogCreator
-});
-
-export const producer = kafka.producer();
-
-export async function conectarKafkaProducer() {
-  await producer.connect();
-}
